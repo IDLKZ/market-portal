ca3a0e2bb1ef557efc368f1ce3e5aa1195c12565
idlkz <idlkz2019@gmail.com>
2020-07-04 18:19:43 +0600

    migration

---
+++ database/migrations/2020_07_04_121515_create_sellers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('img')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
