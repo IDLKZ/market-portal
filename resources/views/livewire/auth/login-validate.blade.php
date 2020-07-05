dc99c5c73945b89291bd8e9f44833a81fe90192e
idlkz <idlkz2019@gmail.com>
2020-07-05 15:19:43 +0600

    migration 22

--- resources/views/livewire/auth/login-validate.blade.php
+++ resources/views/livewire/auth/login-validate.blade.php
@@ -1,10 +1,19 @@
 <div>
+
     <form wire:submit.prevent="submit">
         @if (session()->has('message'))
             <div class="alert alert-info">
                 {{ session('message') }}
             </div>
         @endif
+            <div class="btn-group btn-group-toggle mb-3" data-toggle="buttons">
+                <label class="btn active" role="button" style="border-radius: 0">
+                    <input type="radio" name="type" value="seller" wire:model="type" required="required">Продавец
+                </label>
+                <label class="btn" role="button" style="border-radius: 0">
+                    <input type="radio" name="type" value="client" wire:model="type">Покупатель
+                </label>
+            </div>
         <label class="form-group has-float-label mb-4">
             <input type="text" wire:model="login" class="form-control">
             <span>E-mail / Логин</span>
@@ -21,4 +30,5 @@
             <button type="submit" class="btn btn-primary btn-lg btn-shadow">Вход</button>
         </div>
     </form>
+
 </div>
